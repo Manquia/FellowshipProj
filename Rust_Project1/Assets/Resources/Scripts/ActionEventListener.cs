925a43f8721848aabc8ef1db97333fca8370bd9d
Micah Rust <micahtim@gmail.com>
2017-09-21 09:42:05 -0700

    Added More stuff

---
+++ Rust_Project1/Assets/Resources/Scripts/ActionEventListener.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ActionEventListener : MonoBehaviour
+{
+    public string[] events;
+    private bool[] status;
+
+    bool actionFinished = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        status = new bool[events.Length];
+
+        for (int i = 0; i < events.Length; ++i)
+        {
+            var box = FFMessageBoard<CustomDialogOn>.Box(events[i]);
+            box.Connect(OnCustomEvent);
+        }
+	}
+
+    private void OnCustomEvent(CustomDialogOn e)
+    {
+        int index = 0;
+        for(int i = 0; i < events.Length; ++i)
+        {
+            if (e.tag == events[i])
+                break;
+
+            ++index;
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (actionFinished == true)
+            return;
+
+        CheckEvents();
+	}
+
+    void CheckEvents()
+    {
+
+
+    }
+}
---
+++ Rust_Project1/Assets/Resources/Scripts/TriggerArea.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    public DialogManager.OratorNames SpecifyPerson = DialogManager.OratorNames.None;
+    public string tag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        var character = col.GetComponent<Character>();
+        if (character != null &&
+           (SpecifyPerson == DialogManager.OratorNames.None) /* || @TODO Add non specific character requirement*/)
+        {
+            CustomDialogOn cdo;
+            cdo.tag = tag;
+            var box = FFMessageBoard<CustomDialogOn>.Box(name);
+            box.SendToLocal(cdo);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        var character = col.GetComponent<Character>();
+        if (character != null &&
+           (SpecifyPerson == DialogManager.OratorNames.None) /* || @TODO Add non specific character requirement*/)
+        {
+            CustomDialogOff cdo;
+            cdo.tag = tag;
+            var box = FFMessageBoard<CustomDialogOff>.Box(name);
+            box.SendToLocal(cdo);
+        }
+    }
+}
