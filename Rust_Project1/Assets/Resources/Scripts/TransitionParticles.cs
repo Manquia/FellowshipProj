5b4dbd0238c640a4112ac0087c5e6d4202820df3
Manquia <micahtim@gmail.com>
2017-09-27 17:37:22 -0700

    Made particles work with UI.

---
+++ Rust_Project1/Assets/Resources/Scripts/TransitionParticles.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionParticles : FFComponent {
+    
+    public RectTransform UIRoot;
+
+
+    FFAction.ActionSequence particlesSeq;
+    Dictionary<MenuState, List<RectTransform>> RectsInState = new Dictionary<MenuState, List<RectTransform>>();
+
+    public GameObject[] ParticlesPrefabs;
+    public Vector2 ParticleEmitters;
+
+
+    float maxButtonArea;
+    float minButtonArea;
+
+    void Start()
+    {
+        particlesSeq = action.Sequence();
+
+        FFMessage<PopMenuState>.Connect(OnPopMenuState);
+        FFMessage<PushMenuState>.Connect(OnPushMenuState);
+    }
+    private void OnDestroy()
+    {
+        FFMessage<PopMenuState>.Disconnect(OnPopMenuState);
+        FFMessage<PushMenuState>.Disconnect(OnPushMenuState);
+    }
+
+    private void OnPushMenuState(PushMenuState e)
+    {
+    }
+
+    private void OnPopMenuState(PopMenuState e)
+    {
+        GetComponent<ParticleSystem>().speed
+    }
+
+
+    void ParticlesExplode()
+    {
+
+    }
+    void ParticlesContract()
+    {
+
+    }
+}
