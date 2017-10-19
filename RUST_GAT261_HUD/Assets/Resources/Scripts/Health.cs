39b0338765773e8b0341e5207bff66569c0c9140
Manquia <micahtim@gmail.com>
2017-10-19 12:12:43 -0700

    Added GAT HUD project

---
+++ RUST_GAT261_HUD/Assets/Resources/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct DamageEvent
+{
+	public float delta;
+	public Transform origin;
+}
+
+public class Health : MonoBehaviour {
+
+	public float start;
+	[HideInInspector]public float current;
+	public float max;
+	
+	public bool invulnerable;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		current = start;
+		
+		FFMessageBoard<DamageEvent>.Connect(OnDamageEvent, gameObject);
+	}
+	void OnDestroy()
+	{
+		FFMessageBoard<DamageEvent>.Disconnect(OnDamageEvent, gameObject);
+	}
+	
+	void OnDamageEvent(DamageEvent de)
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
