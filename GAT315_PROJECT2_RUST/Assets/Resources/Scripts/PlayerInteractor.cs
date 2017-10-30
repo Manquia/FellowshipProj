d448c86c156f3c20efaaa8188b7520b8a561fb2d
Micah Rust <micahtim@gmail.com>
2017-10-29 18:45:50 -0700

    Added Player Dot!

---
+++ GAT315_PROJECT2_RUST/Assets/Resources/Scripts/PlayerInteractor.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractor : MonoBehaviour {
+
+    struct ObjectOverRecord
+    {
+        public Transform trans;
+        public int overItemEnumeration;
+    }
+
+    public Transform relative;
+    public Vector3 rayVector = Vector3.forward;
+
+    List<ObjectOverRecord> overObjects = new List<ObjectOverRecord>();
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    int lastOverEnumeration = 0;
+	void FixedUpdate ()
+    {
+        ++lastOverEnumeration;
+
+        bool leftMouseDown = Input.GetMouseButtonUp(0);
+
+
+
+        // Raycast
+        var ray = new Ray(transform.position, transform.TransformVector(rayVector));
+        int rayMask = LayerMask.GetMask("Default");
+        Debug.DrawRay(ray.origin, ray.direction, Color.red); // debug
+        RaycastHit[] raycastHits = Physics.RaycastAll(ray, 100.0F, rayMask);
+
+        //Mouse Over (true)
+        foreach(var hit in raycastHits)
+        {
+            bool alreadyOver = false;
+            // Already Over?
+            for(int i = 0; i < overObjects.Count; ++i)
+            {
+                if(overObjects[i].trans == hit.transform)
+                {
+                    ObjectOverRecord oor;
+                    oor.trans = hit.transform;
+                    oor.overItemEnumeration = lastOverEnumeration;
+
+                    overObjects[i] = oor;
+                    alreadyOver = true;
+                    break;
+                }
+            }
+
+            if (alreadyOver)
+            {
+                continue;
+            }
+            else// We are over a new item!
+            {
+                var interactables = hit.transform.GetComponents<Interactable>();
+                foreach (var inter in interactables)
+                {
+                    Debug.Log("Over New Interactable! True");
+                    inter.MouseOver(true);
+                }
+
+                ObjectOverRecord oor;
+                oor.trans = hit.transform;
+                oor.overItemEnumeration = lastOverEnumeration;
+                overObjects.Add(oor);
+            }
+        }
+
+        // Mouse over (False)
+        for (int i = 0; i < overObjects.Count; ++i)
+        {
+            if (overObjects[i].overItemEnumeration != lastOverEnumeration)
+            {
+                var interactables = overObjects[i].trans.GetComponents<Interactable>();
+                foreach (var inter in interactables)
+                {
+                    inter.MouseOver(false);
+                }
+                Debug.Log("Over New Interactable! False");
+                overObjects.RemoveAt(i);
+            }
+        }
+
+        // clicked with mouse?
+        if (leftMouseDown)
+        {
+            for (int i = 0; i < overObjects.Count; ++i)
+            {
+                var interactables = overObjects[i].trans.GetComponents<Interactable>();
+                foreach (var inter in interactables)
+                {
+                    inter.Use();
+                }
+            }
+        }
+	}
+}
---
+++ GAT315_PROJECT2_RUST/Assets/Resources/Scripts/SentenceController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Interactable
+{
+    void MouseOver(bool active);
+    void Use();
+}
+
+
+[RequireComponent(typeof(UnityEngine.UI.Image))]
+public class SentenceController : MonoBehaviour, Interactable {
+
+    Color startColorSave;
+    public Color OverColor;
+
+    void Start()
+    {
+        var image = GetComponent<UnityEngine.UI.Image>();
+
+        startColorSave = image.color;
+    }
+
+    public void MouseOver(bool active)
+    {
+        var image = GetComponent<UnityEngine.UI.Image>();
+        
+        if (active)
+        {
+            image.color = OverColor;
+        }
+        else // active == false
+        {
+            image.color = startColorSave;
+        }
+    }
+
+    public void Use()
+    {
+        ChooseSentence();
+    }
+
+    void ChooseSentence()
+    {
+
+
+    }
+}
