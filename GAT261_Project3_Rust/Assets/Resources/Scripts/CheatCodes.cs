614b392c0dfbeba1e72d56c916ba75bc674eb7c2
Micah Rust <micahtim@gmail.com>
2017-12-06 22:12:33 -0800

    Added cheat codes

--- GAT261_Project3_Rust/Assets/Resources/Scripts/CheatCodes.cs
+++ GAT261_Project3_Rust/Assets/Resources/Scripts/CheatCodes.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CheatCodes : MonoBehaviour {
 
@@ -11,6 +13,11 @@ public class CheatCodes : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetKey(KeyCode.Alpha1)) SceneManager.LoadScene("Demo 1");
+        if (Input.GetKey(KeyCode.Alpha2)) SceneManager.LoadScene("Demo 2");
+        if (Input.GetKey(KeyCode.Alpha3)) SceneManager.LoadScene("Demo 3");
+
+        if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+    }
 }
